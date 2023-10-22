6cfffc0d000382fb36ca946b63f3c478d9139e38
Fox Walczak <fox.walczak2023@my.ntu.ac.uk>
2023-10-22 22:33:00 +0100

    refactor(view_recipes): clean up

--- view_recipes.py
+++ view_recipes.py
@@ -2,25 +2,32 @@
 # View Recipes #
 #--------------#
 
+from main import display_selection_menu, input_int
 from recipe import Recipe
 from enum import Enum
-from settings import Settings
+from settings import Settings, FilterConditions, SortBys
+from format import format_heading, format_time, format_var_name
 
-def filter_recipes(recipes: list[Recipe], check_include_recipe: function(Recipe)) -> list[int]:
+def filter_recipes(recipes: list[Recipe], check_include_recipe: function(Recipe)) -> list[Recipe]:
     """
-    Add the index of each recipe that satisfies the given filter condition to a list.
+    Add each recipe that satisfies the given filter condition to a list.
 
     Args:
         recipes: The recipes to be filtered.
         check_include_recipe: Used to determine whether a given recipe should be included in the resulting list.
     
     Returns:
-        Indexes of recipes that satisfy the condition(s) checked in check_include_recipe.
+        List of recipes that satisfy the condition(s) checked in `check_include_recipe`.
     """
-    filtered_recipe_indexes = []
-    for i in range(len(recipes)):
-        if check_include_recipe(recipes[i]):
-            filtered_recipe_indexes.append(i)
+    
+
+    filtered_recipes = []
+
+    for r in recipes:
+        if check_include_recipe(recipes[r]):
+            filtered_recipes.append(r)
+
+    return filtered_recipes
 
 def load_files(path: str) -> list[str]:
     """
@@ -47,51 +54,30 @@ def load_recipes() -> list[Recipe]:
     RECIPE_PATH = "recipes.json"
     pass
 
-def check_contains_tags(recipe: Recipe, tags: list[str], union: bool) -> bool:
-    """
-    Check whether `recipe` contains the given `tags`.
-
-    Args:
-        recipe: The recipe to check.
-        tags: Will check whether `recipe` contains these.
-        union: Determines whether to check that `recipe` contains *at least one* of the `tags` (True) or *all* of them (False).
-    
-    Returns:
-        True if it does.
-        False if it does not.
-    """
-    contains_at_least_one_tag = False
-    contains_all_tags = True
-    for tag in tags:
-        if recipe.get_tags().__contains__(tag):
-            contains_at_least_one_tag = True
-        else:
-            contains_all_tags = False
-    return contains_all_tags or (contains_at_least_one_tag and union)
-
-def check_contains_keywords(recipe: Recipe, search_terms: list[str], union: bool) -> bool:
+def deep_search(recipe: Recipe, search_attribute: str, search_terms: list[str], union: bool):
     """
     Check whether `recipe` contains the given `search_terms`.
 
     Args:
-        recipe: The recipe to check.
-        search terms: Will check whether `recipe` contains these.
+        recipe: The `recipe` to check.
+        search_attribute: Where to look for the `search_terms`.
+        search_terms: Will check whether `recipe` contains these.
         union: Determines whether to check that `recipe` contains *at least one* of the `search_terms` (True) or *all* of them (False).
     
     Returns:
         True if it does.
         False if it does not.
     """
-    contains_at_least_one_keyword = False
-    contains_all_keywords = True
-    for keyword in search_terms:
-        if recipe.get_keywords().__contains__(keyword):
-            contains_at_least_one_keyword = True
-        else:
-            contains_all_keywords = False
-    return contains_all_keywords or (contains_at_least_one_keyword and union)
 
-def check_satisfies_filters(recipe: Recipe, filters: list[FilterConditions]) -> bool:
+    count = 0
+
+    for term in search_terms:
+        if term in recipe.__getattribute__(search_attribute):
+            count += 1
+
+    return (count > 0) and ((count == len(search_terms)) or union)
+
+def check_satisfies_filters(recipe: Recipe, settings: Settings) -> bool:
     """
     Check the given `recipe` against every given filter.
 
@@ -103,18 +89,10 @@ def check_satisfies_filters(recipe: Recipe, filters: list[FilterConditions]) -> 
         True if the `recipe` satisfies every filter.
         False if it does not.
     """
-    return ((settings.active_filters.__contains__(FilterConditions.TAGS)      and check_contains_tags(recipe, settings.active_tags, settings.tag_union))
-        and (settings.active_filters.__contains__(FilterConditions.SEARCH)    and check_contains_keywords(recipe, settings.search_terms, settings.keyword_search))
-        and (settings.active_filters.__contains__(FilterConditions.PREP_TIME) and (recipe._prep_time <= settings.max_prep_time)))
 
-def display_recipe(recipe: Recipe):
-    """Display all of the given recipe's attributes."""
-    print(recipe.format_title())
-    print(format_heading("Ingredients"))
-    print(recipe.format_ingredients())
-    print(format_heading("Method"))
-    print(recipe.format_method())
-    print("Tags:", recipe.format_tags())
+    return (((FilterConditions.TAGS      in settings.active_filters) and deep_search(recipe, "_tags", settings.active_tags, settings.tag_union))
+        and ((FilterConditions.SEARCH    in settings.active_filters) and deep_search(recipe, "_search_terms", settings.search_terms, settings.keyword_search))
+        and ((FilterConditions.PREP_TIME in settings.active_filters) and (recipe.get_prep_time() <= settings.max_prep_time)))
 
 class FilterMenuOptions(Enum):
     SORT_BY = 0
@@ -132,14 +110,23 @@ def sort_recipes(recipes: list[Recipe], compare: function) -> list[Recipe]:
     Returns:
         A list of recipes sorted based on the `compare` function.
     """
+
     if len(recipes) == 1:
         return recipes
+    
+    # Split
+
     mid = len(recipes) // 2
     left: list[Recipe] = sort_recipes(recipes[:mid], compare)
     right: list[Recipe] = sort_recipes(recipes[mid:], compare)
-    result: list[Recipe]
+
+    result: list[Recipe] = []
+    
+    # Merge
+
     i: int = 0
     j: int = 0
+    
     while (i + j) < (len(left) + len(right)):
         if compare(left[i], right[j]) < 0:
             result.append(left[i])
@@ -147,16 +134,22 @@ def sort_recipes(recipes: list[Recipe], compare: function) -> list[Recipe]:
         else:
             result.append(right[j])
             j += 1
+
+    # Finish off
+    
     while i < len(left):
         result.append(left[i])
         i += 1
     while j < len(right):
         result.append(right[j])
         j += 1
+    
     return result
 
 def name(variable) -> str:
-    """Returns: the name of the given variable."""
+    """
+    Returns: the name of the given variable.
+    """
     return f'{variable=}'.split('=')[0]
 
 def display_table(rows: list[str], contains_header: bool):
@@ -191,7 +184,7 @@ def format_table(data: list[list[str]], delimiter: str) -> list[str]:
         by the `delimiter`.
     '''
 
-    # Get max length for each column.
+    # Get max length of the values in each column.
 
     max_lengths: list[int]
 
@@ -211,7 +204,7 @@ def format_table(data: list[list[str]], delimiter: str) -> list[str]:
     return table
 
 def format_tag_table_row(fields: list[str], max_tag_length: int, delimiter: str = "    ") -> str:
-    return delimiter.join( [field.ljust(max_tag_length) for field in fields] )
+    return delimiter.join([field.ljust(max_tag_length) for field in fields])
 
 class Commands(Enum):
     ACTIVATE_CURRENT_TAG = "a"
@@ -219,7 +212,7 @@ class Commands(Enum):
     DEACTIVATE_ALL_TAGS = "da"
     ACTIVATE_ONLY_THIS_TAG = "1"
 
-def edit_tags(all_tags: list[str]):
+def edit_tags(tags: list[str], active_tags: list[str]):
     '''
     Allow user to edit which tags are currently active.
     '''
@@ -236,7 +229,7 @@ def edit_tags(all_tags: list[str]):
     tag_table_headers = [ "Tag", "Active?", "Command" ]
     DELIMITER = "    "
     is_active: bool
-    prompt = format_tag_table_row(tag_table_headers[:3], MAX_TAG_LENGTH, DELIMITER) + DELIMITER
+    prompt = format_tag_table_row(tag_table_headers[:-1], MAX_TAG_LENGTH, DELIMITER) + DELIMITER
 
     # Display instructions
 
@@ -245,9 +238,9 @@ def edit_tags(all_tags: list[str]):
 
     # Edit tags
 
-    for tag in all_tags:
+    for tag in tags:
 
-        is_active = active_tags.__contains__(tag)
+        is_active = tag in active_tags
         command = input(prompt)
 
         match command:
@@ -260,32 +253,28 @@ def edit_tags(all_tags: list[str]):
             case "da":
                 active_tags.clear()
             case "1":
-                active_tags = [tag]
+                active_tags.clear()
+                active_tags.append(tag)
 
-def get_max_prep_time() -> int:
+def change_max_prep_time(settings: Settings):
     '''
-    User enters max prep time.
-
-    Returns:
-        New max prep time.
+    User enters max prep time and it is updated in the `settings`.
     '''
     
-    output: str = "Current max prep time:"
+    output: str = "Current max prep time: "
 
-    if max_prep_time == 0:
+    if settings.max_prep_time == 0:
         output += "none"
     else:
-        output += format_time(max_prep_time)
+        output += format_time(settings.max_prep_time)
 
     print(output)
 
     value = input_int(f"Enter new max prep time in minutes (or {EXIT_CHAR} to exit): ", 0)
     if value != EXIT_VALUE:
-        return value
-    else:
-        return max_prep_time
+        settings.max_prep_time = value
 
-def edit_sort_and_filter_settings(settings: Settings) -> Settings:
+def edit_sort_and_filter_settings(settings: Settings):
     """
     Allow user to edit sort and filter settings.
     """
@@ -300,64 +289,77 @@ def edit_sort_and_filter_settings(settings: Settings) -> Settings:
                 settings.sort_by = choice
         
         case FilterMenuOptions.SEARCH_TERMS:
-            user_input = input("Search: ")
+            user_input = input("Search [separate search terms by spaces]: ")
             if user_input != "":
-                settings.search_terms = user_input.split(' ')
+                settings.search_terms = user_input.split(" ")
         
         case FilterMenuOptions.TAGS:
-            edit_tags()
+            edit_tags(settings.all_tags, settings.active_tags)
 
         # For max prep time, user enters a value in minutes, or 0 to remove the constraint.
         case FilterMenuOptions.MAX_PREPARATION_TIME:
-            max_prep_time = get_max_prep_time()
-            
-    return settings
+            change_max_prep_time(settings)
+
+def compare_attribute(left: Recipe, right: Recipe, attribute: str) -> int:
+    """
+    Compare the given `attribute` of the two given recipes.
+
+    Returns:
+        - -1 if `left` < `right`
+        - 0 if `left` = `right`
+        - 1 if `left` > `right`
+    """
+
+    LESS_THAN = -1
+    EQUAL = 0
+    GREATER_THAN = 1
+    
+    if left.__getattribute__(attribute) < right.__getattribute__(attribute):
+        return LESS_THAN
+    elif left.__getattribute__(attribute) == right.__getattribute__(attribute):
+        return EQUAL
+    else:
+        return GREATER_THAN
 
 def view_recipes(recipes: list[Recipe], settings: Settings):
     """
     The view recipes menu option: select a recipe to view it.
     """
+    
+    # Display Current Settings
+
     print(settings.generate_filter_settings_output(NO_MAX_PREP_TIME))
 
-    # User decides whether to edit sort/filter settings.
+    # Edit sort/filter settings
+
     choice = input("Would you like to change these sorting/filtering settings? [Y/N]")
 
     if choice.upper() == "Y":
         edit_sort_and_filter_settings()
-
-    def compare_attribute(left: Recipe, right: Recipe, attribute: str) -> int:
-        """
-        Compare the given `attribute` of the two given recipes.
-
-        Returns:
-          - -1 if `left` < `right`
-          - 0 if `left` = `right`
-          - 1 if `left` > `right`
-        """
-        LESS_THAN = -1
-        EQUAL = 0
-        GREATER_THAN = 1
-        if left.__getattribute__(attribute) < right.__getattribute__(attribute):
-            return LESS_THAN
-        elif left.__getattribute__(attribute) == right.__getattribute__(attribute):
-            return EQUAL
-        else:
-            return GREATER_THAN
 
     compare_functions = {
         SortBys.NAME : lambda left, right: compare_attribute(left, right, f'{Recipe._name=}'.split('=')[0]),
         SortBys.PREP_TIME : lambda left, right: compare_attribute(left, right, f'{Recipe._prep_time=}'.split('=')[0])
     }
 
-    # Filter recipes
+    # Sort and filter
+
     check_recipe_satisfies_filters = lambda recipe: check_satisfies_filters(recipe)
     filtered_recipes = filter_recipes(recipes, check_recipe_satisfies_filters)
     filtered_recipes = sort_recipes(filtered_recipes, compare_functions[settings.sort_by])
     
-    # Display recipes (filtered)
+    # Display
+    
     while recipe_to_display != -1:
-        recipe_to_display = display_selection_menu([recipes[i].get_name() for i in filtered_recipes])
+
+        # User can select which recipe they'd like to view the details of.
+
+        recipe_to_display = display_selection_menu([recipe.format_title() for recipe in filtered_recipes])
         if recipe_to_display == -1:
             break
-        display_recipe(recipes[filtered_recipes[recipe_to_display]])
+
+        # Display details of chosen recipe.
+
+        print(filtered_recipes[recipe_to_display])
+
         input("Enter any key to exit: ")
